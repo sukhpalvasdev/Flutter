883f1827cbc12a26eb627c65b02a18a48d57219d
Sukhpal Vasdev <sukhpal_vasdev@yahoo.com>
2020-05-23 15:04:00 -0700

    upto-Lesson53

--- 01_module/hello_app/lib/main.dart
+++ 01_module/hello_app/lib/main.dart
@@ -1,7 +1,10 @@
 //import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+
+//import './question.dart';
+//import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 //void main(){
 //  runApp(MyApp());
@@ -18,24 +21,50 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final questions = const [
+  final _questions = const [
     {
-      'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Blue', 'Red', 'Green'],
+      'questionText': 'Choose Your Color',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Blue', 'score': 4},
+        {'text': 'Red', 'score': 7},
+        {'text': 'Green', 'score': 5}
+      ],
     },
     {
-      'questionText': 'What\'s your pet\s name?',
-      'answers': ['Dog', 'Cat', 'Rabbit', 'Lion'],
+      'questionText': 'Choose Your Animal',
+      'answers': [
+        {'text': 'Dog', 'score': 3},
+        {'text': 'Dolphin', 'score': 6},
+        {'text': 'Pandas', 'score': 1},
+        {'text': 'Elephant', 'score': 8}
+      ],
     },
     {
-      'questionText': 'What\'s your favourite software?',
-      'answers': ['Mari', 'Katana', 'Houdini', 'Maya'],
+      'questionText': 'Your Eating Pace',
+      'answers': [
+        {'text': 'Speed eater', 'score': 10},
+        {'text': 'Regimented - Pace Driven by Your Schedule', 'score': 7},
+        {'text': 'Chameleon - Adaptive', 'score': 5},
+        {'text': 'Slow eater',  'score': 2}
+      ],
     },
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz(){
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+
+    _totalScore += score;
+
     setState(() {
       _questionIndex += 1;
     });
@@ -46,24 +75,17 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: _questionIndex < questions.length
-              ? Column(
-                  children: [
-                    Question(
-                      questions[_questionIndex]['questionText'],
-                    ),
-                    ...(questions[_questionIndex]['answers'] as List<String>)
-                        .map((answer) {
-                      return Answer(_answerQuestion, answer);
-                    }).toList()
-                  ],
-                )
-              : Center(
-                  child: Text('You did it!'),
-                )),
+        appBar: AppBar(
+          title: Text('Find Devil in You!'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+          answerQuestion: _answerQuestion,
+          questionIndex: _questionIndex,
+          questions: _questions,
+        )
+            : Result(_totalScore, _resetQuiz),
+      ),
     );
   }
 }
